7f764c015553ac435e644da967895c5eed0c7241
eedu7 <mueedahmad067@gmail.com>
2025-12-31 22:32:14 +0500

    Added the configuration file

---
+++ backend/core/config.py
@@ -0,0 +1,23 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Config(BaseSettings):
+    # Supabase
+    SUPABASE_PROJECT_REF: str = ""
+    SUPABASE_SECRET_KEY: str = ""
+    SUPABASE_ISSUER: str = ""
+
+    # Database
+    DATABASE_URL: str = ""
+
+    # JWKS
+    JWKS_URL: str = ""
+
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8',
+        case_sensitive=True,
+        extra='ignore'
+    )
+
+config = Config()
