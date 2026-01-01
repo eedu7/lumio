b9fcdeec4027554899c93acb5325ab2a122d6d44
eedu7 <mueedahmad067@gmail.com>
2026-01-01 11:34:26 +0500

    updated

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from backend!")
+from api import router
+from fastapi import FastAPI
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+app.include_router(router)
