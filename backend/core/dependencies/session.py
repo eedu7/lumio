961514c748f1914a0ba043c0e8e9bcc7da6860d7
eedu7 <mueedahmad067@gmail.com>
2026-01-01 11:33:56 +0500

    Updated the session dependency, make it backward compatibalbe'

---
+++ backend/core/dependencies/session.py
@@ -0,0 +1,9 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.database import get_db
+
+SessionDep= Annotated[AsyncSession, Depends(get_db)]
+"""Dependency for getting a database session."""
