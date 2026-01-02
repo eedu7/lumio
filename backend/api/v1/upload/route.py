72832fdb0d9267fc1a282ab84cb53383c49fdaba
eedu7 <mueedahmad067@gmail.com>
2026-01-02 08:25:09 +0500

    created the upload api

--- backend/api/v1/upload/route.py
+++ backend/api/v1/upload/route.py
@@ -1,9 +1,22 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Query, status
 
 from core.dependencies import SessionDep
+from core.models import ProductImage
 
 router = APIRouter()
 
 
-@router.post("/")
-async def upload_file(session: SessionDep): ...
+@router.post("/", status_code=status.HTTP_200_OK)
+async def upload_file(
+    session: SessionDep,
+    product_id: Annotated[str | None, Query()] = None,
+):
+    # TODO: Create image url
+
+    # TODO: Save image in supabase bucket
+    new_product_image = ProductImage(image_url="")
+
+    session.add(new_product_image)
+    await session.commit()
